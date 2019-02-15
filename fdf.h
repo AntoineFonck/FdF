2aca40f855bb8826004d3f406d831f1edacb8890
Antoine Fonck <afonck@MacBook-Pro-de-Antoine.local>
2019-02-15 19:14:13 +0100

    compiled library and reviewed fdf.h file

--- fdf.h
+++ fdf.h
@@ -10,10 +10,28 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-typedef struct	s_coor
+#ifndef FDF_H
+# define FDF_H
+# include "./minilibx_macos/mlx.h"
+# include "./libft/libft.h"
+# include <math.h>
+# define WIN_WIDTH 500
+# define WIN_HEIGHT 500
+
+typedef struct	s_img
 {
-	int x;
-	int y;
-	void *mlx_ptr;
-	void *win_ptr;
-}				t_coor;
+	void	*img_ptr;
+	int	*data;
+	int	size_l;
+	int	bpp;
+	int	endian;
+}		t_img
+
+typedef struct	s_mlx
+{
+	void	*mlx_ptr;
+	void	*win_ptr;
+	t_img	img;
+}		t_mlx;
+
+#endif
