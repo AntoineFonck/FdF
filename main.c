9fefb7c948a947c277e6498c3babe6d213a39aa0
Antoine FONCK <afonck@e1r10p5.42.fr>
2019-02-25 16:58:47 +0100

    draw line function

--- main.c
+++ main.c
@@ -6,26 +6,51 @@
 /*   By: afonck <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/14 12:30:57 by afonck            #+#    #+#             */
-/*   Updated: 2019/02/14 15:59:28 by afonck           ###   ########.fr       */
+/*   Updated: 2019/02/25 16:53:48 by afonck           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
-#include "mlx.h"
 #include <stdlib.h>
+
+void	save_line(int x1, int y1, int x2, int y2, int *data)
+{
+	int x;
+	int y;
+
+	if (x1 <= x2 && (x2 - x1) >= abs(y2 - y1))
+	{
+		x = x1;
+		while (x <= x2)
+		{
+			data[x * WIN_HEIGHT + (y1 + ((y2 - y1) * (x - x1)) / (x2 - x1))] = 0xFF69B4;
+			x++;
+		}
+	}
+	else
+	{
+		y = y1;
+		while (y <= y2)
+		{
+			data[y * WIN_WIDTH + (x1 + ((x2 - x1) * (y - y1)) / (y2 - y1))] = 0xFFFFFF;
+			y++;
+		}
+	}
+}
 
 int		main()
 {
 	t_mlx	mlx;
-	int	count_w;
-	int	count_h;
+	//int	count_w;
+	//int	count_h;
 
-	count_h = 0;
+	//count_h = 0;
 	mlx.mlx_ptr = mlx_init();
 	mlx.win_ptr = mlx_new_window(mlx.mlx_ptr, WIN_WIDTH, WIN_HEIGHT, "Just To Try");
 	mlx.img.img_ptr = mlx_new_image(mlx.mlx_ptr, WIN_WIDTH, WIN_HEIGHT);
 	mlx.img.data = (int *)mlx_get_data_addr(mlx.img.img_ptr, &mlx.img.bpp, &mlx.img.size_l, &mlx.img.endian);
 
+	/*
 	while (count_h < WIN_HEIGHT)
 	{
 		count_w = 0;
@@ -39,6 +64,8 @@ int		main()
 		}
 		count_h++;
 	}
+	*/
+	save_line(0, 0, 40, 40, mlx.img.data);
 	mlx_put_image_to_window(mlx.mlx_ptr, mlx.win_ptr, mlx.img.img_ptr, 0, 0);
 	mlx_loop(mlx.mlx_ptr);
 	return (0);
--- reader.c
+++ reader.c
@@ -1,37 +1,15 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   fdf.h                                              :+:      :+:    :+:   */
+/*   reader.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: afonck <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/02/14 15:11:28 by afonck            #+#    #+#             */
-/*   Updated: 2019/02/25 13:07:59 by afonck           ###   ########.fr       */
+/*   Created: 2019/02/25 13:16:29 by afonck            #+#    #+#             */
+/*   Updated: 2019/02/25 14:00:45 by afonck           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef FDF_H
-# define FDF_H
-# include "./minilibx_macos/mlx.h"
-# include "./libft/libft.h"
-# include <math.h>
-# define WIN_WIDTH 500
-# define WIN_HEIGHT 500
+#include "fdf.h"
 
-typedef struct	s_img
-{
-	void	*img_ptr;
-	int	*data;
-	int	size_l;
-	int	bpp;
-	int	endian;
-}		t_img;
 
-typedef struct	s_mlx
-{
-	void	*mlx_ptr;
-	void	*win_ptr;
-	t_img	img;
-}		t_mlx;
-
-#endif
