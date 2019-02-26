4b642214f2a6bdf482596c66455498ad392871df
Antoine FONCK <afonck@e1r4p7.42.fr>
2019-02-26 11:22:16 +0100

    trying to read

--- reader.c
+++ reader.c
@@ -6,7 +6,7 @@
 /*   By: afonck <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/25 13:16:29 by afonck            #+#    #+#             */
-/*   Updated: 2019/02/25 17:42:37 by afonck           ###   ########.fr       */
+/*   Updated: 2019/02/26 11:21:56 by afonck           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,21 @@
 
 char	**check_and_read(char *arg)
 {
-	char *line;
+	char	**tab;
+	char	*line;
+	int		ret;
+	int		max_h;
 
-	fd = open(arg, O_RDONLY);
-	while (get_next_line(fd, &line) > 0)
+	max_h = 0;
+	if ((fd = open(arg, O_RDONLY)) == -1)
+		return (NULL);
+	while ((ret = get_next_line(fd, &line)) > 0)
+	{
+		ft_strdup(tab[max_h], line);
+		ft_memdel(&line);
+		max_h++;
+	}
+	if (ret == 0)
+		return (tab);
+	return (NULL);
 }
