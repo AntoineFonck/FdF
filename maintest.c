d874b3665eb3a90efa2c0fdadccca1e1797e7eb9
Antoine Fonck <afonck@MBP-de-Antoine.fritz.box>
2019-02-28 17:59:36 +0100

    Ugly but the parsing WORKS, and doesn't LEAK and useful comments

--- maintest.c
+++ maintest.c
@@ -9,32 +9,57 @@ int main(int argc, char **argv)
 	int j;
 
 	if ((map = malloc(sizeof(t_map))) == NULL)
+	{
+		printf("problem with map malloc");
 		return (1);
+	}
 	i = 0;
 	if (argc != 2)
+	{
+		printf("not good number of arguments");
 		return (1);
-	tabchar = check_and_read(argv[1], map);
-	printf("check and read works\n");
+	}
+	if ((tabchar = check_and_read(argv[1], map)) != NULL)
+		printf("check and read works\n");
+	else
+		printf("problem with check and read\n");
 	atoi_tab(tabchar, map);
-	printf("atoi tab works\n");
+	if (map->tab != NULL)
+		printf("atoi tab works\n");
+	else
+		printf("problem with atoi tab\n");
 	while (i < map->h_max)
 	{
 		while (j < map->w_max)
 		{
 			printf("printing tab[%d][%d]", i, j);
-			ft_putnbr(map->tab[i][j]);
+			//ft_putnbr(map->tab[i][j]);
+			printf("%d ", map->tab[i][j]);
+			//write(1, " ", 1);
 			j++;
 		}
 		write(1, "\n", 1);
+		if (j != map->w_max)
+			printf("error with j=%d and map->w_max=%d\n", j, map->w_max);
 		j = 0;
 		i++;
 	}
+	if (i != map->h_max)
+		printf("error with i index=%d and map->h_max=%d\n", i, map->h_max);
 	i = 0;
 	while (i < map->h_max)
 	{
-		ft_memdel((void **)&tabchar[i]);
+		if (tabchar[i])
+			ft_memdel((void **)&tabchar[i]);
+		else
+			printf("couldn't memdel tabchar[%d]\n", i);
 		i++;
 	}
-	ft_memdel((void **)tabchar);
+	if (tabchar)
+		ft_memdel((void **)tabchar);
+	else
+		printf("couldn't memdel tabchar\n");
+	printf("map->w_max = %d and map->h_max = %d\n", map->w_max, map->h_max);
+	while (1) {}
 	return (0);
 }
--- reader.c
+++ reader.c
@@ -11,6 +11,7 @@
 /* ************************************************************************** */
 
 #include "fdf.h"
+#include <stdio.h>
 
 void	count_nb_lines(char *arg, t_map *map)
 {
@@ -20,7 +21,10 @@ void	count_nb_lines(char *arg, t_map *map)
 	int fd;
 
 	if ((fd = open(arg, O_RDONLY)) == -1)
+	{
+		ft_putstr("problem with open in countnblines\n");
 		return ;
+	}
 	nblines = 0;
 	i = 0;
 	while (get_next_line(fd, &line) > 0)
@@ -30,7 +34,10 @@ void	count_nb_lines(char *arg, t_map *map)
 	}
 	ft_memdel((void **)&line);
 	if ((close(fd)) == -1)
+	{
+		ft_putstr("problem with close in countnblines\n");
 		return ;
+	}
 	map->h_max = nblines;
 }
 
@@ -44,18 +51,29 @@ char	**check_and_read(char *arg, t_map *map)
 	i = 0;
 	count_nb_lines(arg, map);
 	if ((tab = (char **)malloc(sizeof(*tab) * (map->h_max))) == NULL)
+	{
+		ft_putstr("problem with checkandread malloc for tab\n");
 		return (NULL);
+	}
 	if ((fd = open(arg, O_RDONLY)) == -1)
+	{
+		ft_putstr("problem with open in checkandread\n");
 		return (NULL);
+	}
 	while (get_next_line(fd, &line) > 0)
 	{
 		tab[i] = ft_strdup(line);
+		if (tab[i] == NULL)
+			printf("problem with strdup line to tab[%d]\n", i);
 		ft_memdel((void **)&line);
 		i++;
 	}
 	ft_memdel((void **)&line);
 	if ((close(fd)) == -1)
+	{
+		ft_putstr("problem with close in checkandread\n");
 		return (NULL);
+	}
 	return (tab);
 }
 
@@ -68,22 +86,37 @@ void	atoi_tab(char **tabchar, t_map *map)
 
 	map->w_max = 0;
 	if ((map->tab = (int **)malloc(sizeof(int*) * map->h_max)) == NULL)
+	{
+		ft_putstr("problem with malloc map->tab in atoi_tab");
 		return ;
+	}
 	i = 0;
 	while (i < map->h_max)
 	{
 		temp = countwords(tabchar[i], ' ');
 		if (temp > map->w_max)
-			map->w_max = temp;
-		if ((map->tab[i] = (int *)malloc(sizeof(int) * (temp))) == NULL)
-			return ;
-		tmp = ft_strsplit(tabchar[i], ' ');
-		while (j < (countwords(tabchar[i], ' ')))
 		{
-			map->tab[i][j] = ft_atoi(tmp[j]);
-			j++;
+			printf("temp is equal to %d for i=%d\n", temp, i);
+			map->w_max = temp;
+		}
+		if ((map->tab[i] = (int *)malloc(sizeof(int) * (temp))) == NULL)
+		{
+			printf("problem with malloc map->tab[%d] in atoi_tab\n", i);
+			return ;
+		}
+		if ((tmp = ft_strsplit(tabchar[i], ' ')) == NULL)
+		{
+			printf("problem with strsplit when i=%d in atoi_tab\n", i);
+			return ;
 		}
 		j = 0;
+		while (j < map->w_max/*(countwords(tabchar[i], ' '))*/)
+		{
+			map->tab[i][j] = ft_atoi(tmp[j]);
+			ft_memdel((void **)&tmp[j]);
+			j++;
+		}
+		ft_memdel((void **)&tmp);
 		i++;
 	}
 }
