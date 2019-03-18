17385deac4762206e89d2538d1e538fb1207f51f
Antoine FONCK <afonck@e1r8p3.42.fr>
2019-03-18 15:56:57 +0100

    improving points filling

--- main.c
+++ main.c
@@ -6,25 +6,25 @@
 /*   By: afonck <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/14 12:30:57 by afonck            #+#    #+#             */
-/*   Updated: 2019/02/25 17:10:05 by afonck           ###   ########.fr       */
+/*   Updated: 2019/03/18 15:53:04 by afonck           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
 #include <stdio.h>
 
-void	fill_pix(int *data, int x, int y, int w_max, int color)
+void	fill_pix(int *data, int x, int y, /*int w_max, */int color)
 {
-	data[x + (y * w_max * 10)] = color;
+	data[x + y/*(y * w_max * 10)*/] = color;
 }
 
 void	save_line(int x1, int y1, int x2, int y2, int *data)
 {
 	int x;
 	int y;
-	int offset;
+	//int offset;
 
-	offset = 10;
+	//offset = 10;
 
 	if (x1 <= x2 && (x2 - x1) >= abs(y2 - y1))
 	{
@@ -50,6 +50,8 @@ void		put_coor_in_data(t_map *map, int *data)
 {
 	int x;
 	int y;
+	double myconst = 0.6;
+	double myconst2 = 1;
 	int offset;
 	int i;
 	int j;
@@ -59,14 +61,13 @@ void		put_coor_in_data(t_map *map, int *data)
 	offset = 10;
 	while (i < map->h_max/*y < (map->h_max * offset)*/)
 	{
-		//x = ((offset * map->w_max) / 2) + offset;
-		x = 0;
+		//x = 0;
+		x = WIN_WIDTH / 2;
 		j = 0;
 		while (j < map->w_max/*x < (map->w_max * offset)*/)
 		{
-			//data[(x - y) + ((map->w_max * offset * 20) * ((x + y) / 2)) + (offset * map->w_max * map->h_max * map->tab[i][j])] = 0xFFFFFF;
 			//save_line((x - y), ((map->w_max * offset * 20) * ((x + y) / 2)), (x + offset) - y, (((x +  offset) + y) / 2), data, offset);
-			fill_pix(data, x, y, map->w_max, 0xFFFFFF);
+			fill_pix(data, (myconst * x - myconst2 * y), ((map->tab[i][j] + (myconst / 2) * x + (myconst2 / 2) * y) * WIN_WIDTH), 0xFFFFFF);
 			x += offset;
 			j++;
 		}
@@ -81,10 +82,7 @@ int		main(int argc, char **argv)
 	t_map	*map;
 	char	**tabchar;
 	int	i;
-	//int	count_w;
-	//int	count_h;
 
-	//count_h = 0;
 	i = 0;
 	if (argc != 2)
 		return (1);
@@ -97,28 +95,9 @@ int		main(int argc, char **argv)
 	atoi_tab(tabchar, map);
 	mlx.mlx_ptr = mlx_init();
 	mlx.win_ptr = mlx_new_window(mlx.mlx_ptr, WIN_WIDTH, WIN_HEIGHT, "Just To Try");
-	mlx.img.img_ptr = mlx_new_image(mlx.mlx_ptr, (map->w_max * 10/* * 200*/), (map->h_max * 10/* * 200*/)/*WIN_WIDTH, WIN_HEIGHT*/);
+	mlx.img.img_ptr = mlx_new_image(mlx.mlx_ptr, WIN_WIDTH, WIN_HEIGHT);
 	mlx.img.data = (int *)mlx_get_data_addr(mlx.img.img_ptr, &mlx.img.bpp, &mlx.img.size_l, &mlx.img.endian);
 
-	/*
-	while (count_h < WIN_HEIGHT)
-	{
-		count_w = 0;
-		while (count_w < WIN_WIDTH)
-		{
-			if (count_w % 2)
-				mlx.img.data[count_h * WIN_WIDTH + count_w] = 0xFFFFFF;
-			else
-				mlx.img.data[count_h * WIN_WIDTH + count_w] = 0xFF0000;
-			count_w++;
-		}
-		count_h++;
-	}
-	*/
-	//save_line(0, 0, 100, 0, mlx.img.data);
-	//save_line(2, 2, 1, 100, mlx.img.data);
-	//save_line(100, 100, 100, 150, mlx.img.data);
-	//save_line(100, 100, 100, 150, mlx.img.data);
 	put_coor_in_data(map, mlx.img.data);
 	while (i < 208)
 	{
@@ -126,7 +105,7 @@ int		main(int argc, char **argv)
 		i++;
 	}
 	printf("mlx.img.data[%d] = %d\n", i, mlx.img.data[i]);
-	mlx_put_image_to_window(mlx.mlx_ptr, mlx.win_ptr, mlx.img.img_ptr, 100, 100);
+	mlx_put_image_to_window(mlx.mlx_ptr, mlx.win_ptr, mlx.img.img_ptr, 0, 0);
 	mlx_loop(mlx.mlx_ptr);
 	return (0);
 }
