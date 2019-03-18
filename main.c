b793bcbf38f4966e119db2a80552f1e433b790de
Stina LUTZENKIRCHEN <sluetzen@e1r8p5.42.fr>
2019-03-18 15:55:55 +0100

    LINES WOOOOORK

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: afonck <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/14 12:30:57 by afonck            #+#    #+#             */
-/*   Updated: 2019/02/25 17:10:05 by afonck           ###   ########.fr       */
+/*   Updated: 2019/03/18 15:54:18 by sluetzen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,22 +26,102 @@ void	save_line(int x1, int y1, int x2, int y2, int *data)
 
 	offset = 10;
 
-	if (x1 <= x2 && (x2 - x1) >= abs(y2 - y1))
+	if (x1 <= x2 && (x2 - x1) >= (y2 - y1))
 	{
 		x = x1;
 		while (x <= x2)
 		{
-			data[x + (y1 + ((y2 - y1) * (x - x1)) / (x2 - x1))] = 0xFF69B4;
+			data[x + WIN_HEIGHT * (y1 + (((y2 - y1) + (x - x1)) / (x2 - x1)))] = 0xFF69B4;
 			x++;
 		}
 	}
 	else
 	{
-	y = y1;
-		while (y <= y2)
+		y = y1;
+		if (y < y2)
 		{
-			data[y + (x1 * (((x2 - x1) + (y - y1)) / (y2 - y1)))] = 0xFFFFFF;
-			y++;
+			while (y <= y2)
+			{
+				data[y * WIN_WIDTH * (x1 + (((x2 - x1) + (y - y1)) / (y2 - y1)))] = 0xFFFFFF;
+				y++;
+			}
+		}
+		else
+		{
+			while (y >= y2)
+			{
+				data[y * WIN_WIDTH * (x1 + (((x2 - x1) + (y - y1)) / (y2 - y1)))] = 0xFFFFFF;
+				y--;
+			}
+		}
+	}
+}
+
+int absolute(int i)
+{
+	if (i < 0)
+		return(-i);
+	else
+		return(i);
+}
+
+void paint_line(int x1, int y1, int x2, int y2, int *data)
+{
+	int x;
+	int y;
+	int dx;
+	int dy;
+	int i;
+	int xinc;
+	int yinc;
+	int cumul;
+	x = x1;
+	y = x2;
+	dx = x2 - x1;
+	dy = y2 - y1;
+	if (dx > 0)
+		xinc = 1;
+	else
+		xinc = -1;
+	if (dy > 0)
+		yinc = 1;
+	else
+		yinc = -1;
+	dx = absolute(dx);
+	dy = absolute(dy);
+	data[x + y * WIN_WIDTH] = 0xFF69B4;
+	if (dx > dy)
+	{
+		cumul = dx / 2;
+		i = 1;
+		while (i <= dx)
+		{
+			x += xinc;
+			cumul += dy;
+			if (cumul >= dx)
+			{
+				cumul -= dx;
+				y += yinc;
+			}
+			data[x + y * WIN_WIDTH] = 0xFF00FF;
+			i++;
+		}
+	}
+	else
+	{
+		cumul = dy / 2;
+		i = 1;
+		while (i <= dy)
+		{
+			y += yinc;
+			cumul += dx;
+			if (cumul >= dy)
+			{
+				cumul -= dy;
+				x += xinc;
+			}
+			data[x + y * WIN_WIDTH] = 0x00FF00;
+			i++;
 		}
 	}
 }
@@ -97,29 +177,29 @@ int		main(int argc, char **argv)
 	atoi_tab(tabchar, map);
 	mlx.mlx_ptr = mlx_init();
 	mlx.win_ptr = mlx_new_window(mlx.mlx_ptr, WIN_WIDTH, WIN_HEIGHT, "Just To Try");
-	mlx.img.img_ptr = mlx_new_image(mlx.mlx_ptr, (map->w_max * 10/* * 200*/), (map->h_max * 10/* * 200*/)/*WIN_WIDTH, WIN_HEIGHT*/);
+	mlx.img.img_ptr = mlx_new_image(mlx.mlx_ptr, WIN_WIDTH, WIN_HEIGHT);
 	mlx.img.data = (int *)mlx_get_data_addr(mlx.img.img_ptr, &mlx.img.bpp, &mlx.img.size_l, &mlx.img.endian);
 
 	/*
-	while (count_h < WIN_HEIGHT)
-	{
-		count_w = 0;
-		while (count_w < WIN_WIDTH)
-		{
-			if (count_w % 2)
-				mlx.img.data[count_h * WIN_WIDTH + count_w] = 0xFFFFFF;
-			else
-				mlx.img.data[count_h * WIN_WIDTH + count_w] = 0xFF0000;
-			count_w++;
-		}
-		count_h++;
-	}
-	*/
-	//save_line(0, 0, 100, 0, mlx.img.data);
-	//save_line(2, 2, 1, 100, mlx.img.data);
-	//save_line(100, 100, 100, 150, mlx.img.data);
-	//save_line(100, 100, 100, 150, mlx.img.data);
-	put_coor_in_data(map, mlx.img.data);
+	   while (count_h < WIN_HEIGHT)
+	   {
+	   count_w = 0;
+	   while (count_w < WIN_WIDTH)
+	   {
+	   if (count_w % 2)
+	   mlx.img.data[count_h * WIN_WIDTH + count_w] = 0xFFFFFF;
+	   else
+	   mlx.img.data[count_h * WIN_WIDTH + count_w] = 0xFF0000;
+	   count_w++;
+	   }
+	   count_h++;
+	   }
+	   */
+	paint_line(3, 2, 18, 11, mlx.img.data); // test
+	paint_line(2, 2, 60, 60, mlx.img.data); // test
+	paint_line(100, 100, 0, 100, mlx.img.data); // test
+	paint_line(100, 100, 100, 0, mlx.img.data); // test
+	//put_coor_in_data(map, mlx.img.data);
 	while (i < 208)
 	{
 		printf("data[%d] = %d\n", i, mlx.img.data[i]);
