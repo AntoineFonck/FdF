bfab44f4e89f13e19a2f2bce8677da2997cd17b0
Antoine FONCK <afonck@e1r11p3.42.fr>
2019-03-25 17:39:03 +0100

    merge again for main.c

--- main.c
+++ main.c
@@ -6,11 +6,7 @@
 /*   By: afonck <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/14 12:30:57 by afonck            #+#    #+#             */
-<<<<<<< HEAD
-/*   Updated: 2019/03/25 16:29:06 by afonck           ###   ########.fr       */
-=======
-/*   Updated: 2019/03/25 14:35:45 by sluetzen         ###   ########.fr       */
->>>>>>> refs/remotes/origin/master
+/*   Updated: 2019/03/25 17:38:43 by afonck           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,18 +27,7 @@ int		main(int argc, char **argv)
 	map->win_ptr = mlx_new_window(map->mlx_ptr, WIN_WIDTH, WIN_HEIGHT, "FdF");
 	map->img.img_ptr = mlx_new_image(map->mlx_ptr, WIN_WIDTH, WIN_HEIGHT);
 	map->img.data = (int *)mlx_get_data_addr(map->img.img_ptr, &map->img.bpp, &map->img.size_l, &map->img.endian);
-<<<<<<< HEAD
-	map->offset = 2;
-	map->change_alt = 0.1;
-	map->const1 = 1;
-	map->const2 = 1;
-	map->start_point.x = WIN_WIDTH / 2;
-	map->start_point.y = 0;
-	map->view = 1;
-	//map->menu = 1;
-=======
 	init_map(map);
->>>>>>> refs/remotes/origin/master
 	trace_all(map);
 	mlx_put_image_to_window(map->mlx_ptr, map->win_ptr, map->img.img_ptr, 0, 0);
 	mlx_key_hook(map->win_ptr, &all, map);
